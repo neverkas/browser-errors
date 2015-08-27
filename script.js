c5d8d6df42e09d394f4a99bdb1456e72c528e965
desconocido <desconocido@desconocido-desktop.(none)>
2015-08-27 15:14:12 -0300

    Se agregó primer version del script

---
+++ script.js
@@ -0,0 +1,31 @@
+(function(){
+	function logErrors(){
+		this.list = [];
+	}
+
+	logErrors.prototype.save = function(msg, url, line){
+		var error 				= {};
+		var datetime 			= new Date();
+
+				error.datetime = datetime.toLocaleString();
+
+				error.message = msg;
+				error.url 		= url;
+				error.line 		= line;
+
+		this.list.push(error)
+	}
+
+	logErrors.prototype.getAll = function(){
+		console.log(this.list);
+	}
+
+	window.onerror = function (msg, url, line) {
+		logs = new logErrors();
+		logs.save(msg, url, line);		
+
+		logs.getAll();
+	}
+})();
+
+console.log(a);
